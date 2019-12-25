f00f286c9fd3d136819dec3a01fb885622200c5c
fyagiz <furkanyagiz@gmail.com>
2019-12-25 16:42:32 +0200

    Update binary_handle.py
    Two's complement and i_type handler is added.

--- binary_handle.py
+++ binary_handle.py
@@ -42,19 +42,51 @@ def r_type(s,t,d,shamt,fun):
 def i_type(opcode,s,t,imm):
     s = s.replace("0b","")
     t = t.replace("0b","")
-    imm = bin(imm)
-    imm = imm.replace("0b","")
+
+    # Check immediate value is negative or not
+    if imm >= 0:
+        imm = bin(imm)
+        imm = imm.replace("0b","")
+        if len(imm) < 16:
+            imm = imm.zfill(16-len(imm) + len(imm))
+    else:
+        imm = bin(imm)
+        imm = imm.replace("-0b","")
+        if len(imm) < 16:
+            imm = imm.zfill(16-len(imm) + len(imm))     
+        imm = twos_complement(imm,16)   
+        
     if len(s) < 5:
         s = s.zfill(5-len(s) + len(s)) 
     if len(t) < 5:
         t = t.zfill(5-len(t) + len(t))
-    if len(imm) < 16:
-        imm = imm.zfill(16-len(imm) + len(imm))
+
     final = opcode + s + t + imm
     final = int(final,2)
     final = hex(final).replace("0x", "")
     final = last_check(final)
     return final
+
+# To take Two's Complement
+def twos_complement(imm, bit_length):
+    imm_list = list(imm)
+    i = bit_length-1
+    flag = 0
+    while (i>=0):
+        if flag == 0:
+            if imm_list[i] == '0':
+                pass
+            else:
+                flag=1
+        else:
+            if imm_list[i] == '0':
+                imm_list[i] = '1'
+            else:
+                imm_list[i] = '0'
+        i = i-1
+    imm = ''.join(map(str,imm_list))
+    return imm
+
 # To check Leading Zeros
 def last_check(final):
     if len(final) < 8:
